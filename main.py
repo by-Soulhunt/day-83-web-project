0b273b60496526dc85b241eb031560fac40b1361
by-Soulhunt <mydronmail@gmail.com>
2025-02-28 12:44:09 +0200

    Add data in footer.html, edit links and img profile in index.html, create Data Base

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text, ForeignKey
+import datetime
 import os
 
 
@@ -7,6 +11,76 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 Bootstrap(app)
+
+
+# Create DataBase
+class Base(DeclarativeBase):
+    """Declarative class"""
+    pass
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main_db.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# Configure tables
+class Projects(db.Model):
+    """
+    Projects DB table
+    """
+    __tablename__ = "projects"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+class Experience(db.Model):
+    """
+    Experience DB table
+    """
+    __tablename__ = "experience"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    position: Mapped[str] = mapped_column(String(100), nullable=False)
+    organization: Mapped[str] = mapped_column(String(250), nullable=False)
+    org_address: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+class Education(db.Model):
+    """
+    Education DB table
+    """
+    __tablename__ = "education"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    education_institution: Mapped[str] = mapped_column(String(100), nullable=False)
+    institution_address: Mapped[str] = mapped_column(String(250), nullable=False)
+    major: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+class Skills(db.Model):
+    """
+    Skills DB table
+    """
+    __tablename__ = "skills"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    skills: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+class Language(db.Model):
+    """
+    Language DB table
+    """
+    __tablename__ = "language"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    language: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 # Routs
@@ -44,6 +118,14 @@ def contact():
     :return: template contact.html
     """
     return render_template("contact.html")
+
+
+@app.context_processor
+def inject_date():
+    """
+    :return: Add date variable into all templates
+    """
+    return {"date":datetime.datetime.now().year}
 
 
 if __name__ == '__main__':
